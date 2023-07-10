ab5950b038cb4a4b6880d3975d82ab37641f2755
OvsyankinVI <ovsyankin717@gmail.com>
2023-07-10 18:51:43 +0400

    my project

---
+++ src/components/app/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="navbar orange lighten-1">
+    <div class="nav-wrapper">
+      <div class="navbar-left">
+        <a  href="#">
+          <i class="material-icons black-text" @click="$emit('click-on')">dehaze</i>
+        </a>
+        <span class="black-text">{{ date }}</span>
+      </div>
+
+      <ul class="right hide-on-small-and-down">
+        <li>
+          <router-link
+            to="/profile"
+            class="dropdown-trigger black-text"
+            href="#"
+            data-target="dropdown"
+            ref="dropdown"
+          >
+            USER NAME
+          </router-link>
+
+          <ul id="dropdown" class="dropdown-content">
+            <li>
+              <router-link to="/profile" class="black-text">
+                <i class="material-icons">account_circle</i>Профиль
+              </router-link>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <router-link to="" class="black-text">
+                <i class="material-icons">assignment_return</i>Выйти
+              </router-link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      date: '',
+      interval: null,
+      format: ''
+    }
+  },
+  mounted () {
+    this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000)
+  },
+  beforeUnmount () {
+    clearInterval(this.interval)
+  },
+  filters: {
+    dateFilter: function (value) {
+      console.log(value)
+    }
+  }
+}
+</script>
