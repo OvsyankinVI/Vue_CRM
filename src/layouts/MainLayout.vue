75063d162b7d7d024305dca30c4361e795cdec21
OvsyankinVI <717ovsyankin@gmail.com>
2023-07-17 19:00:46 +0400

    less-9_add name in NavBar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,6 +30,11 @@ export default {
       isopen: true
     }
   },
+  async mounted () {
+    if (!Object.keys(this.$store.getters.info).length) {
+      await this.$store.dispatch('fetchInfo')
+    }
+  },
   components: {
     navbar, sidebar
   }
